fefd74c904ab7aede874af99d81b8cf9e35f6c5a
Vinzenz Fuhrmann <vinzenz.f@gmx.de>
2021-11-06 00:53:52 +0100

    Update guard

--- dashboard/src/app/guards/auth.guard.ts
+++ dashboard/src/app/guards/auth.guard.ts
@@ -1,13 +1,15 @@
+import { AuthState } from './../states/auth.state';
 import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
+import { Store } from '@ngxs/store';
 import { AuthService } from '../services/auth.service';
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-  constructor(private auth: AuthService, private router: Router) {}
+  constructor(private auth: AuthService, private router: Router, private store: Store) {}
 
   canActivate(): boolean {
-    if (!this.auth.isAuthenticated) {
+    if (!this.store.selectSnapshot(AuthState.isLoggedIn)) {
       this.router.navigate(['login']);
       return false;
     }
