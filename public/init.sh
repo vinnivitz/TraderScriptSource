358ae2c2db0960a897e94bb8cc64baae965497f0
Vinzenz Fuhrmann <vinzenz.f@gmx.de>
2021-11-17 22:39:51 +0100

    Update data path

--- public/init.sh
+++ public/init.sh
@@ -1,7 +1,7 @@
 read -p "Enter a name for the docker image [trading-coach]:" docker_image
 docker_image=${docker_image:-"trading-coach"}
 docker build -t $docker_image ./data
-cp startup.sh .
+cp ./data/startup.sh .
 rm -r data
 rm init.sh
 rm init.cmd
