84145386c4d5d7c40b506eeb81d0091fa71c35d0
VinniVitz <34209870+VinniVitz@users.noreply.github.com>
2021-11-21 12:41:52 +0100

    Update init.cmd

--- public/init.cmd
+++ public/init.cmd
@@ -1,7 +1,7 @@
 @echo off
 set docker_image=
-set /p docker_image=Enter a name for the docker image [trading-coach]:
-if "x%docker_image%x" == "xx" set docker_image=trading-coach
+set /p docker_image=Enter a name for the docker image [traderscript]:
+if "x%docker_image%x" == "xx" set docker_image=traderscript
 docker build -t %docker_image% .\data
 xcopy ..\data\startup.cmd .
 rmdir /s /q data
